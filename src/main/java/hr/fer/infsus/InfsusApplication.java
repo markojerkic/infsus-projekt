28ca8650901842708955201e0a03732fe8877d19
Marko Jerkić <markojerkic266@gmail.com>
2024-05-06 19:25:31 +0200

    init

---
+++ src/main/java/hr/fer/infsus/InfsusApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.infsus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfsusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfsusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/infsus/InfsusApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.infsus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfsusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
